c4a8c73280a7dfc12753e7f606ef3bf1415a8426
sseezov <sseezov@mail.ru>
2022-05-11 11:22:00 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import data from "./data";
+function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (count <= 0) {
+      setText(data.slice(0, 1));
+    } else setText(data.slice(0, amount));
+  };
+
+  return (
+    <section className="section-center">
+      <h3>tired of boding lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        ></input>
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
+}
+
+export default App;
